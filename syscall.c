80ce6a1db4012e6b8b2cc01c287f14878fce0840
Alireza Karimi <alireza.k.7880@gmail.com>
2021-10-22 00:55:54 -0700

    externing system calls in syscall.c

--- syscall.c
+++ syscall.c
@@ -103,6 +103,8 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_getProcCount(void);
+extern int sys_getReadCount(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
